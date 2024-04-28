146671eddc3a717e61d3ea9cef0fce800654dd25
RyanSnelleksz <118699845+RyanSnelleksz@users.noreply.github.com>
2024-04-28 20:51:07 +1000

    Finished Q7
    Added jump module and functionality.

---
+++ modules/game_jump/jump.lua
@@ -0,0 +1,79 @@
+
+-- Window functions
+
+hidden = true
+function init() -- Start up logic
+  g_ui.importStyle('jump')
+
+  jumpMenuButton = modules.client_topmenu.addLeftGameButton('jumpMenuButton', tr('Jump Menu'), '/images/topbuttons/questlog', toggle)
+ 
+  jumpWindow = g_ui.createWidget('JumpWindow', rootWidget)
+  
+  button = jumpWindow:getChildByIndex(1)
+  
+  jumpUpdate()
+  
+  hide()
+
+  connect(g_game, { onGameEnd = destroyWindows})
+end
+
+function terminate() -- Close logic
+  disconnect(g_game, { onGameEnd = destroyWindows})
+
+  destroy()
+  jumpMenuButton:destroy()
+end
+
+function destroy() -- Clear the window
+  if jumpWindow then
+    jumpWindow:destroy()
+    jumpWindow = nil
+  end
+end
+
+function hide()
+  jumpWindow:hide()
+  hidden = true
+end
+
+function show()
+  jumpWindow:show()
+  jumpWindow:raise()
+  jumpWindow:focus()
+  hidden = false
+end
+
+function toggle() -- Used to switch between diding and showing the window
+  if jumpWindow:isVisible() then
+    hide()
+  else
+    show()
+  end
+end
+
+function jumpUpdate()
+if hidden == false then
+local x = button:getX()
+local y = button:getY()
+
+if x > 250 then
+
+button:move(x - 10, y)
+
+else
+
+jump()
+
+end
+
+end
+return scheduleEvent(modules.game_jump.jumpUpdate, 100)
+end
+
+function jump()
+
+local rand = math.random(110, 440)
+
+button:move(500, rand)
+end
